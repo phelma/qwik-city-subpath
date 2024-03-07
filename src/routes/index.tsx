9d19096cc3d0db11093e1f8f14586e957454f074
Phil Helm <git@phelm.co.uk>
2024-03-07 16:20:33 +0000

    add express adapter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,14 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, useSignal } from '@builder.io/qwik'
 
 export default component$(() => {
+  const counter = useSignal(0)
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
-  );
-});
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
+    <div>
+      Qwik demo
+      <br />
+      <button onClick$={() => (counter.value += 1)}>
+        Clicked {counter.value} times
+      </button>
+    </div>
+  )
+})
